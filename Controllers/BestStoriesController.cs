38b7e5faaf3e0a7569d5d637d070470d1aa5e740
Manish Gupta <32765339+manishgupta31286@users.noreply.github.com>
2023-07-27 11:59:23 +0200

    Add files via upload

---
+++ Controllers/BestStoriesController.cs
@@ -0,0 +1,64 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Caching.Memory;
+using Newtonsoft.Json;
+using WebApplication2.Model;
+
+namespace WebApplication2.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class BestStoriesController : ControllerBase
+    {
+        private readonly HttpClient _httpClient;
+        private readonly IMemoryCache _memoryCache;
+
+        public BestStoriesController(HttpClient httpClient, IMemoryCache memoryCache)
+        {
+            _httpClient = httpClient;
+            _memoryCache = memoryCache;
+        }
+
+        public async Task<IEnumerable<Stories>> Get(int firstN)
+        {
+            if (_memoryCache.TryGetValue("bestStories", out string response))
+            {
+
+            }
+            else
+            {
+                response = await _httpClient.GetStringAsync("https://hacker-news.firebaseio.com/v0/beststories.json");
+
+                _memoryCache.Set("bestStories", response);
+            }
+
+            var bestStories = JsonConvert.DeserializeObject<IEnumerable<string>>(response);
+
+            var firstNBestStories = bestStories.Take(firstN);
+
+            List<Stories> stories = new List<Stories>();
+
+            foreach (var item in firstNBestStories)
+            {
+                stories.Add(await GetStories(item));
+            }
+
+            return stories;
+        }
+
+        private async Task<Stories> GetStories(string id)
+        {
+            if (_memoryCache.TryGetValue($"Stories{id}", out string response))
+            {
+
+            }
+            else
+            {
+                response = await _httpClient.GetStringAsync($"https://hacker-news.firebaseio.com/v0/item/{id}.json");
+
+                _memoryCache.Set($"Stories{id}", response);
+            }
+            return JsonConvert.DeserializeObject<Stories>(response.ToString());
+        }
+    }
+}
---
+++ Model/Stories.cs
@@ -0,0 +1,15 @@
+﻿namespace WebApplication2.Model
+{
+    public class Stories
+    {
+        public string by { get; set; }
+        public int descendants { get; set; }
+        public int id { get; set; }
+        public int[] kids { get; set; }
+        public int score { get; set; }
+        public int time { get; set; }
+        public string title { get; set; }
+        public string type { get; set; }
+        public string url { get; set; }
+    }
+}
